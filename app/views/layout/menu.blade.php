0429aa2bc296b5b102b81f91dabb3f5a2266dd19
jess <kelvinodesk@gmail.com>
2014-04-15 01:46:12 +0800

    updated menu

--- app/views/layout/menu.blade.php
+++ app/views/layout/menu.blade.php
@@ -13,11 +13,10 @@
             <li>
                 <a href="#">File</a>
                 <ul class="cbp-hssubmenu">
-                    <li><a href="/president/create"><img src="/assets/images/avatar.svg" alt="img01" /><span>President</span></a></li>
-                    <li><a href="#"><img src="/assets/images/avatar.svg" alt="img02"/><span>Vice President</span></a></li>
-                    <li><a href="#"><img src="/assets/images/avatar.svg" alt="img03"/><span>Treasurer</span></a></li>
-                    <li><a href="#"><img src="/assets/images/avatar.svg" alt="img04"/><span>Secretary</span></a></li>
-                    <li><a href="#"><img src="/assets/images/avatar.svg" alt="img05"/><span>Representative</span></a></li>
+                    <li><a href="/president/create"><img src="/assets/images/avatar.svg" alt="img01" /><span>Course</span></a></li>
+                    <li><a href="#"><img src="/assets/images/avatar.svg" alt="img02"/><span>Student</span></a></li>
+                    <li><a href="#"><img src="/assets/images/avatar.svg" alt="img03"/><span>Position</span></a></li>
+                    <li><a href="#"><img src="/assets/images/avatar.svg" alt="img04"/><span>Candidate</span></a></li>
                     <li><a href="#"><img src="/assets/images/avatar.svg" alt="img06"/><span>User</span></a></li>
                     <li><a href="#"><img src="/assets/images/lock.png" alt="img06"/><span>Generate Password</span></a></li>
                     <li><a href="/profile"><img src="/assets/images/avatar.svg" alt="img06"/><span>Profile</span></a></li>
