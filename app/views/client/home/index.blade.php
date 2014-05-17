d35bb7975287f6bd44c890810fe741374f9a81b8
kmligue <kelvinodesk@gmail.com>
2014-05-17 14:17:08 +0800

    updated home

--- app/views/client/home/index.blade.php
+++ app/views/client/home/index.blade.php
@@ -1,29 +1,32 @@
 @extends('layout.main2')
 
 @section('content')
-	
-	@foreach($positions as $position)
-		@if($position->course_id == null || $position->course_id == $student->course_id)
-			<div class="list-group" style="margin-top: 20px;">
-				<a class="list-group-item active">
-					<h2>{{ $position->name }}</h2>
-				</a>
+	{{ Form::open(array('url' => 'home/create', 'method' => 'get', 'style' => 'margin-bottom: 20px;')) }}
+		@foreach($positions as $position)
+			@if($position->course_id == null || $position->course_id == $student->course_id)
+				<div class="list-group" style="margin-top: 20px;">
+					<a class="list-group-item active">
+						<h2>{{ $position->name }}</h2>
+					</a>
 
-				@foreach($candidates as $candidate)
-					@if($candidate->position_id == $position->id)
-						<a class="list-group-item">
-							<input type="radio" id="{{ $candidate->id }}" name="{{ $position->name }}" style="width: 1.5em; height: 1.5em;">
-							<label for="{{ $candidate->id }}">
-								<img class="img-circle" src="{{ $candidate->imagepath }}" style="width: 125px;">
-								{{ $candidate->fname . ' ' . $candidate->mname . ' ' . $candidate->lname }}
-							</label>
-						</a>
-					@endif
-				@endforeach	
-			</div>	
-		@endif
-	@endforeach
+					@foreach($candidates as $candidate)
+						@if($candidate->position_id == $position->id)
+							<a class="list-group-item">
+								<input type="radio" id="{{ $candidate->id }}" name="{{ $position->name }}" style="width: 1.5em; height: 1.5em;">
+								<label for="{{ $candidate->id }}">
+									<img class="img-circle" src="{{ $candidate->imagepath }}" style="width: 125px;">
+									{{ $candidate->fname . ' ' . $candidate->mname . ' ' . $candidate->lname }}
+								</label>
+							</a>
+						@endif
+					@endforeach	
+				</div>	
+			@endif
+		@endforeach
 
+		<input type="submit" value="Submit" class="btn btn-danger" style="float: right;">
+		<div style="clear: both;"></div>
+	{{ Form::close() }}
 @stop
 
 @section('script')
